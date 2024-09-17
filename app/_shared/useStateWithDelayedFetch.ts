00baf1da4e36c7e9628ad0c8c2dd82fffa36c057
Bryan W. Kao <bryan.w.kao@gmail.com>
2024-09-17 14:56:10 -0500

    comments

--- app/_shared/useStateWithDelayedFetch.ts
+++ app/_shared/useStateWithDelayedFetch.ts
@@ -1,6 +1,13 @@
 'use client'
 import { useState, useRef, useEffect } from 'react'
 
+/** 
+ * Manages state for a controlled input component while batching [mutation] changes.
+ * Optionally allows for immediate mutation.
+ * @param {Function} mutateFn - the mutation function
+ * @param {Type} initialData - data to initialize state
+ * @param {number} timeoutDelay - timeout duration in milliseconds (ms)
+ */
 export function useStateWithDelayedFetch<Type>(mutateFn: Function, initialData: Type, timeoutDelay: number) {
     const [isPending, setIsPending] = useState(false)
     const [error, setError] = useState<Error | undefined>()
