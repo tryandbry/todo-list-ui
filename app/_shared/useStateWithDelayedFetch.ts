febca90b6eefd2f0e9c7b733077a34ef1d9e410d
Bryan W. Kao <bryan.w.kao@gmail.com>
2024-09-24 15:11:45 -0500

    add guard clause to delayedFetch hook to prevent unnecessary server updates

--- app/_shared/useStateWithDelayedFetch.ts
+++ app/_shared/useStateWithDelayedFetch.ts
@@ -12,11 +12,23 @@ export function useStateWithDelayedFetch<Type>(mutateFn: Function, initialData: 
     const [isPending, setIsPending] = useState(false)
     const [error, setError] = useState<Error | undefined>()
     const [data, setData] = useState(initialData)
+    const prevData = useRef<Type>(initialData)
     const timeout = useRef<ReturnType<typeof setTimeout> | undefined>()
     const timeoutDuration = useRef(timeoutDelay)
 
+    // store previous [data] state to prevent unnecessary PATCH requests
     useEffect(() => {
+        prevData.current = data
+    }, [data])
+
+    useEffect(() => {
+        // guard clause to prevent unnecessary updates if data has not changed.
+        if (data == prevData.current) {
+            return
+        }
+
         timeout.current = setTimeout(async () => {
+            console.log("delayed fetch useEffect timeout callback")
             setIsPending(true)
             try {
                 await mutateFn(data)
