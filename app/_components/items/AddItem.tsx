c38e7263c60261534c3c39e2b02e3940c3ee6318
Bryan W. Kao <bryan.w.kao@gmail.com>
2024-09-23 21:05:23 -0500

    handle pending and error states

--- app/_components/items/AddItem.tsx
+++ app/_components/items/AddItem.tsx
@@ -1,13 +1,33 @@
 'use client'
 
-import { useState, ChangeEvent, KeyboardEvent } from "react"
+import { useState, useEffect, ChangeEvent, KeyboardEvent } from "react"
+import classNames from "classnames"
 
 import { useCreateItem } from "./hooks/useCreateItem"
 import { Item } from "@/app/_shared/types"
 
 export default function AddItem() {
     const [inputText, setInputText] = useState('')
-    const { mutate: createItem } = useCreateItem()
+    const [showError, setShowError] = useState(false)
+    const { mutate: createItem, reset, isPending, isError, isSuccess } = useCreateItem()
+    const inputClasses = classNames(
+        "outline-none grow",
+        { "cursor-progress bg-slate-100 text-slate-500": isPending },
+    )
+    const inputDivClasses = classNames(
+        "flex items-center border-y-2 border-r-2 border-slate-100 text-black text-sm rounded-r-md py-1 px-2",
+        { "cursor-progress bg-slate-100": isPending, "bg-white": !isPending },
+    )
+    const inputDivDivClasses = classNames(
+        "bg-cyan-500 rounded-md pl-1",
+        { "animate-pulse": isPending },
+    )
+    const errorSpanClasses = classNames(
+        "bg-red-500 text-xs px-2 ease-in-out duration-300",
+        //TODO: make error transition fade
+        { "opacity-0": !showError },
+        { "collapse": !showError },
+    )
     const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
         setInputText(event.target.value)
     }
@@ -18,20 +38,34 @@ export default function AddItem() {
 
         const item: Item = { name: inputText }
         createItem(item)
-        setInputText('')
     }
+
+    useEffect(() => {
+        if (isError) {
+            console.error("Unable to create item")
+            setShowError(true)
+            setTimeout(() => setShowError(false), 3000)
+        }
+        if (isError || isSuccess) {
+            reset()
+            setInputText('')
+        }
+    }, [isError, isSuccess, reset])
+
     return (
         <div className="flex flex-col mb-2">
-            <div className="bg-cyan-500 rounded-md pl-1">
-                <div className="flex items-center bg-white border-y-2 border-r-2 border-slate-100 text-black text-sm rounded-r-md py-1 px-2">
+            <div className={inputDivDivClasses}>
+                <div className={inputDivClasses}>
                     <input type="text" name="" id="" placeholder="Enter new todo item"
+                        disabled={isPending}
                         value={inputText}
                         onChange={handleOnChange}
                         onKeyDown={handleOnKeyDown}
-                        className="outline-none grow"
+                        className={inputClasses}
                     ></input>
                 </div>
             </div>
+            <span className={errorSpanClasses}>Unable to create item</span>
         </div>
     )
 }
